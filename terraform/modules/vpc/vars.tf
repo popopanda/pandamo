5b2c824a4423255b3e58b63eb32d8ef800337f27
popopanda <leung.jeff1120@gmail.com>
2017-12-19 16:46:59 -0800

    vpc

--- terraform/modules/vpc/vars.tf
+++ terraform/modules/vpc/vars.tf
@@ -1,4 +1,4 @@
-variable "cidr_block" {
+variable "vpc_cidr_block" {
   default = "10.0.0.0/16"
 }
 
@@ -8,4 +8,16 @@ variable "enable_dns_support" {
 
 variable "enable_dns_hostnames" {
   default = "yes"
+}
+
+variable "zone" {
+  default = ["a", "b", "c", "e"]
+}
+
+variable "public_cidr_block" {
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "private_cidr_block" {
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
 }
